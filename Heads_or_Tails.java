a13a7165c655ffeb39e79df092f823002c31d4ad
momopon1415 <fortnitet-hyaku@outlook.jp>
2022-07-14 17:12:26 +0900

    Final revision

--- Heads_or_Tails.java
+++ Heads_or_Tails.java
@@ -1,10 +1,15 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Heads_or_Tails {
     public static void main(String[] args) {
         Random random = new Random();
         int Heads = 0;
         int Tails = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Who are you?");
+        String username = scanner.next();
+        System.out.println("Hello, " + username + "!");
         System.out.println("Tossing a coin...");
         for (int i = 1; i < 4; i++) {
             int randomValue = random.nextInt(2);
@@ -19,8 +24,8 @@ public class Heads_or_Tails {
         }
 
         System.out.println("Heads: " + Heads + " Tails: " + Tails);
-        if(Heads > Tails) System.out.println("You won");
-        else System.out.println("You lose");
+        if(Heads > Tails) System.out.println(username + " won");
+        else System.out.println(username + "lose");
 
     }
 }
